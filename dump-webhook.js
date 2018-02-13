5b3bfbcf9f2fe99792972ceff8cc801fabb30c0e
kdao <khanh.dao@move.com>
2018-02-13 11:41:42 -0800

    Merge heads

--- dump-webhook.js
+++ dump-webhook.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 
 const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
 const APIAI_TOKEN = process.env.APIAI_TOKEN;
+const WEATHER_API_KEY = process.env.WEATHER_API_KEY;
 const WEBHOOK_TOKEN = process.env.WEBHOOK_TOKEN;
 
 console.log(PAGE_ACCESS_TOKEN);
@@ -19,7 +20,7 @@ const server = app.listen(process.env.PORT || 5000, () => {
   console.log('Express server listening on port %d in %s mode', server.address().port, app.settings.env);
 });
 
-const apiaiApp = apiai(APIAI_TOKEN);
+// const apiaiApp = apiai(APIAI_TOKEN);
 
 app.get('/', (req, res) => {
   res.status(200).send('361069427');
@@ -35,8 +36,10 @@ app.get('/webhook', (req, res) => {
   }
 });
 
-/* Handling all messenges */
+// This is to test chat bot
 app.post('/webhook', (req, res) => {
+  // console.log(req.body);
+  // const myID = '1575752172537936';
   if (req.body.object === 'page') {
     req.body.entry.forEach((entry) => {
       entry.messaging.forEach((event) => {
@@ -55,33 +58,19 @@ function sendMessage(event) {
   let sender = event.sender.id;
   let text = event.message.text;
 
-  let apiai = apiaiApp.textRequest(text, {
-    sessionId: 'tabby_cat' // use any arbitrary id
+  request({
+    url: 'https://graph.facebook.com/v2.6/me/messages',
+    qs: {access_token: PAGE_ACCESS_TOKEN},
+    method: 'POST',
+    json: {
+      recipient: {id: sender},
+      message: {text: text}
+    }
+  }, function (error, response) {
+    if (error) {
+        console.log('Error sending message: ', error);
+    } else if (response.body.error) {
+        console.log('Error: ', response.body.error);
+    }
   });
-
-  apiai.on('response', (response) => {
-    // Got a response from api.ai. POST to Facebook Messenger
-    let aiText = response.result.fulfillment.speech;
-    request({
-      url: 'https://graph.facebook.com/v2.6/me/messages',
-      qs: {access_token: PAGE_ACCESS_TOKEN},
-      method: 'POST',
-      json: {
-        recipient: {id: sender},
-        message: {text: aiText}
-      }
-    }, (error, response) => {
-      if (error) {
-          console.log('Error sending message: ', error);
-      } else if (response.body.error) {
-          console.log('Error: ', response.body.error);
-      }
-    });
-  });
-
-  apiai.on('error', (error) => {
-    console.log(error);
-  });
-
-  apiai.end();
 }
